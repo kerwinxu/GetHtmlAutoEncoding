e4ffc9e92c47a35c877141a8cf16016644f1735a
kerwinxu <kerwin.cn@gmail.com>
2017-07-13 19:36:49 +0800

    v1.0:已经找到可以自动识别编码的方法了。

--- GetHtmlAutoEncoding/Form1.Designer.cs
+++ GetHtmlAutoEncoding/Form1.Designer.cs
@@ -28,12 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.flowLayoutPanel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txtResult);
+            this.splitContainer1.Size = new System.Drawing.Size(692, 487);
+            this.splitContainer1.SplitterDistance = 33;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.txtUrl);
+            this.flowLayoutPanel1.Controls.Add(this.btnStart);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(692, 33);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(3, 3);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(437, 21);
+            this.txtUrl.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(446, 3);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "开始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(3, 3);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResult.Size = new System.Drawing.Size(686, 435);
+            this.txtResult.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(692, 487);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "下载网页并自动识别编码";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            this.splitContainer1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtResult;
     }
 }
 
--- GetHtmlAutoEncoding/Form1.cs
+++ GetHtmlAutoEncoding/Form1.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
+using System.Net;
 using System.Text;
 using System.Windows.Forms;
 
@@ -13,6 +15,46 @@ namespace GetHtmlAutoEncoding
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //string strTemp = "网址：" + txtUrl.Text + ",的编码：" + getEndoding(txtUrl.Text)+Environment.NewLine;
+            txtResult.AppendText(GetHtml(txtUrl.Text));
+        }
+
+        /// <summary>
+        /// 取得编码
+        /// </summary>
+        /// <param name="strUrl"></param>
+        private string getEndoding(string strUrl)
+        {
+
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(strUrl);
+            req.Method = "GET";
+            string str;
+            HttpWebResponse Stream = req.GetResponse() as HttpWebResponse;
+            return Stream.CharacterSet;
+
+        }
+        /// <summary>
+        /// 下载网页并自动根据编码转成字符串格式。
+        /// </summary>
+        /// <param name="strUrl"></param>
+        /// <returns></returns>
+        private string GetHtml(string strUrl)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(strUrl);
+            req.Method = "GET";
+            string str;
+            HttpWebResponse Stream = req.GetResponse() as HttpWebResponse;
+            string strEncoding = Stream.CharacterSet;
+            using (StreamReader reader = new StreamReader(Stream.GetResponseStream(), System.Text.Encoding.GetEncoding(strEncoding)))
+            {
+                str = reader.ReadToEnd();
+                return str;
+            }
+            return null;
         }
     }
 }
